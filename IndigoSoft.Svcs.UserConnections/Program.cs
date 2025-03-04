fa111090ff959bbd40bbae95e2c160135313f7f2
arodenkov <arodenkov@yandex.ru>
2025-03-04 23:55:50 +0300

    add some files

--- IndigoSoft.Svcs.UserConnections/Program.cs
+++ IndigoSoft.Svcs.UserConnections/Program.cs
@@ -1,6 +1,22 @@
+// <copyright file="Program.cs" company="IndigoSoft">
+// Copyright © 2025 IndigoSoft
+// </copyright>
+
+using NLog;
+using NLog.Web;
+
+// Early init of NLog to allow startup and exception logging, before host is built
+// Loads nlog configuration. Loads either nlog.config or nlog.{environment}.config.
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Setup NLog.
+builder.Logging.ClearProviders();
+builder.Host.UseNLog();
+
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
